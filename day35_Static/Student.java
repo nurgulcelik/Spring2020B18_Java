e029682a8b61b28c86355be40004748f19da76b8
nurgulcelik <nurgulcelik3478@gmail.com>
2020-05-07 13:07:43 -0400

    day35 class

---
+++ day35_Static/Student.java
@@ -0,0 +1,19 @@
+package day35_Static;
+
+public class Student {//for CybertekSchool
+    String name;
+    int id;
+    double gpa;
+    static String school="Cybertek";
+    public void setInfo(String name,int id,double gpa){
+        this.name=name;
+        this.id=id;
+        this.gpa=gpa;
+    }
+    public static void printSchoolName(){
+        System.out.println("School name is "+school);
+    }
+    public String toString(){
+        return "Name:"+name+" School name: "+school;
+    }
+}
